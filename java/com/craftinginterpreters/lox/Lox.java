87b2268cc58a750756ce3977163073c0ade07050
FerreirAndre <adrecruz15@gmail.com>
2025-03-19 15:43:57 -0300

    finalizing scanner

--- java/com/craftinginterpreters/lox/Lox.java
+++ java/com/craftinginterpreters/lox/Lox.java
@@ -7,9 +7,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;
-
-import static sun.security.util.Event.report;
 
 public class Lox {
 
