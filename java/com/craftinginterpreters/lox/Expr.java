488b84c7ae9da139e8bf95456bdc3cea2da3acd7
FerreirAndre <adrecruz15@gmail.com>
2025-03-22 22:39:46 -0300

    creating the base script that generates the Expr

---
+++ java/com/craftinginterpreters/lox/Expr.java
@@ -0,0 +1,43 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+
+abstract class Expr {
+        static class Binary {
+                Binary(Expr left, Token operator, Expr right) {
+                        this.left = left;
+                        this.operator = operator;
+                        this.right = right;
+                }
+
+                final Expr left;
+                final Token operator;
+                final Expr right;
+        }
+
+        static class Grouping {
+                Grouping(Expr expression) {
+                        this.expression = expression;
+                }
+
+                final Expr expression;
+        }
+
+        static class Literal {
+                Literal(Object value) {
+                        this.value = value;
+                }
+
+                final Object value;
+        }
+
+        static class Unary {
+                Unary(Token operator, Expr right) {
+                        this.operator = operator;
+                        this.right = right;
+                }
+
+                final Token operator;
+                final Expr right;
+        }
+}
