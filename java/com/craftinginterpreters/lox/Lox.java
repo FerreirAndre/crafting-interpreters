88d5f6971fb8fb0ef8b8ae825e4249f9b611ec13
FerreirAndre <adrecruz15@gmail.com>
2025-03-24 23:01:50 -0300

    fixing the generateast and adding more stuff to the script.

--- java/com/craftinginterpreters/lox/Lox.java
+++ java/com/craftinginterpreters/lox/Lox.java
@@ -21,16 +21,18 @@ public class Lox {
             runPrompt();
         }
     }
+
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError)
             System.exit(65);
     }
+
     private static void runPrompt() throws IOException {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
-        for (;;){
+        for (;;) {
             System.out.print("> ");
             String line = reader.readLine();
             if (line == null)
@@ -39,14 +41,18 @@ public class Lox {
             hadError = false;
         }
     }
+
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
-        for (Token token : tokens) System.out.println(token);
+        for (Token token : tokens)
+            System.out.println(token);
     }
+
     static void error(int line, String message) {
         report(line, "", message);
     }
+
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
